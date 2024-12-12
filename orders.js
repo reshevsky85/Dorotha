dd58887462c594836399ae003a2de1429aa5b06b
reshevsky85 <162910754+reshevsky85@users.noreply.github.com>
2024-12-12 11:58:09 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$("#batonik").click(function () {
+		$("#oils_per_supplier").slideToggle("slow");
+	});
+	$("#batonik2").click(function () {
+		$("#oils_per_supplier2").slideToggle("slow");
+	});
+});
